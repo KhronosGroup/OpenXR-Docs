0d422b97386204e1caab88b192d494a002537856
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-07-31 13:00:20 -0500

    OpenXR Specification 1.0.18 (2021-07-30)
    The main changes in this release include clarifications to the Session
    chapter of the specification, plus the addition of diagrams to the text
    describing grip and aim pose. The release also adds one new ratified KHR
    extension (promoted from a vendor extension), and a number of new vendor
    extensions.
    
    -   Registry
        -   Add ratified XR_KHR_swapchain_usage_input_attachment_bit Khronos
            extension. (Promotion of
            XR_MND_swapchain_usage_input_attachment_bit, which is now
            deprecated.) (internal MR 2045)
        -   Add new XR_FB_foveation, XR_FB_foveation_configuration, and
            XR_FB_foveation_vulkan vendor extensions. (internal MR 2050)
        -   Add additional extension dependencies to
            XR_FB_swapchain_update_state. (internal MR 2072, internal issue
            1572)
        -   Add new XR_FB_composition_layer_secure_content vendor extension.
            (internal MR 2075)
        -   Add new XR_FB_composition_layer_alpha_blend vendor extension.
            (internal MR 2078)
        -   Add new XR_FB_composition_layer_image_layout vendor extension.
            (internal MR 2090)
        -   Add new XR_MSFT_spatial_anchor_persistence vendor extension.
            (internal MR 2093)
        -   Add some simple Schematron rules and a script to check the XML
            registry against them. (internal MR 2103)
        -   Register author ID and reserve vendor extensions for Unity.
            (internal MR 2105)
        -   Reserve extension ID range 187-196 for LIV Inc. (internal MR
            2102)
    -   Specification
        -   Rewrite the introduction of Session chapter to explain a typical
            session lifecycle. (internal MR 2087)
        -   Add grip diagram to specification (internal MR 2092, internal
            issue 1545, OpenXR-Docs PR 45, OpenXR-Docs issue 44)
        -   Describe how runtimes may register themselves at installation
            time for manual selection. (internal MR 2081, internal MR 2109,
            internal issue 1574)
        -   Document new ratified
            XR_KHR_swapchain_usage_input_attachment_bit Khronos extension.
            (Promotion of XR_MND_swapchain_usage_input_attachment_bit, which
            is now deprecated.) (internal MR 2045)
        -   Document new XR_FB_foveation, XR_FB_foveation_configuration, and
            XR_FB_foveation_vulkan vendor extensions. (internal MR 2050)
        -   Document new XR_FB_composition_layer_secure_content vendor
            extension. (internal MR 2075)
        -   Document new XR_FB_composition_layer_alpha_blend vendor
            extension. (internal MR 2078)
        -   Document new XR_FB_composition_layer_image_layout vendor
            extension. (internal MR 2090)
        -   Document new XR_MSFT_spatial_anchor_persistence vendor
            extension. (internal MR 2093)
        -   Minor cleanups to the loader documentation/specification.
            (internal MR 2108)
        -   Revise Varjo vendor extensions so that the sample code is
            buildable (and build during CI testing). (internal MR 2020)
        -   Scripts: Verify externsync attributes of destroy calls.
            (internal MR 2065)
        -   Update XR_MSFT_spatial_anchor to note the external sync
            requirements of the destroy call. (internal MR 2065)
        -   Add missing copyright/license notice for FB extension sources
            (internal MR 2076, internal issue 1562)
        -   Add missing item to new enum constants of spatial anchors
            extension (internal MR 2088)
        -   scripts: Handle aliased bitmask values properly in header files.
            (internal MR 2045)

---
+++ specification/checkSchematron.sh
@@ -0,0 +1,70 @@
+#!/bin/sh -e
+#
+# Copyright (c) 2019-2021, The Khronos Group Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# checkSchematron.sh - Validate the registry XML against the Schematron
+# rules, using https://github.com/schxslt/schxslt/
+#
+# Usage: checkSchematron.sh
+
+set -e
+
+URL=https://github.com/schxslt/schxslt/releases/download/v1.7.3/schxslt-cli.jar
+SCHXSLT_CLI=registry/$(basename "$URL")
+
+XML=registry/xr.xml
+SCH=registry/registry.sch
+REPORT=registry/report.srvl
+
+
+(
+    cd "$(dirname $0)"
+
+    if ! command -v wget > /dev/null; then
+        echo "Skipping schematron validation: no wget."
+        exit 0
+    fi
+
+    if ! command -v java > /dev/null; then
+        echo "Skipping schematron validation: no java."
+        exit 0
+    fi
+    if ! command -v grep > /dev/null; then
+        echo "Skipping schematron validation: no grep. (!?)"
+        exit 0
+    fi
+
+    if [ ! -f "$SCHXSLT_CLI" ]; then
+        echo "Trying to download SchXslt..."
+        wget $URL -O "$SCHXSLT_CLI" || true
+    fi
+
+
+    if [ ! -f "$SCHXSLT_CLI" ]; then
+        echo "Skipping schematron validation: download of SchXslt failed."
+        exit 0
+    fi
+
+    java -jar "$SCHXSLT_CLI" -d $XML -s $SCH -v -o $REPORT
+
+    if grep -q "failed-assert>" $REPORT; then
+        echo "Failed assert detected, exiting with error"
+        exit 1
+    fi
+
+    exit 0
+)
